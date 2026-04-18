615dbe39dd7ac8b540ffbd22f9c63801a1f5196d
Bijaya Lama <blama4991@gmail.com>
2026-04-18 23:33:29 +0000

    Complete Task 1 memory read/write functions

--- include/lc3vm.h
+++ include/lc3vm.h
@@ -1,9 +1,9 @@
 /** @file lc3vm.h
  * @brief LC-3 VM API
  *
- * @author Student Name
- * @note   cwid: 123456
- * @date   Spring 2024
+ * @author Bijaya Lama
+ * @note   cwid: 50425524
+ * @date   Spring 2026
  * @note   ide:  g++ 8.2.0 / GNU Make 4.2.1
  *
  * Header include file for LC-3 simulator API/functions.
@@ -66,6 +66,8 @@ extern uint16_t reg[];
 extern uint16_t PC_START;
 
 // your task functions should go here
+uint16_t mem_read(uint16_t address);
+void mem_write(uint16_t address, uint16_t value);
 
 void rti(uint16_t i);
 void res(uint16_t i);
--- src/assg03-tests.cpp
+++ src/assg03-tests.cpp
@@ -1,9 +1,9 @@
 /** @file assg03-tests.cpp
  * @brief Unit tests for LC-3 microarchitecture/VM simulator.
  *
- * @author Student Name
- * @note   cwid: 123456
- * @date   Spring 2024
+ * @author Bijaya Lama
+ * @note   cwid: 50425524
+ * @date   Spring 2026
  * @note   ide:  VS Code Editor / IDE ; g++ 8.2.0 / GNU Make 4.2.1
  *
  * Unit tests for assignment 03, implementaiton of LC-3
@@ -15,7 +15,7 @@
 #include "lc3vm.h"
 #include "lc3vm_dbg.h"
 
-#undef task1
+#define task1
 #undef task2
 #undef task3
 #undef task4
@@ -145,24 +145,16 @@ TEST_CASE("Task 3: DR destination register extraction operations", "[task3]")
 }
 
 TEST_CASE("Task 3: SEXTIMM immediate operand extraction and sign extension", "[task3]")
-{
-  CHECK(SEXTIMM(add_ins) == 0x0003);
-}
+{ CHECK(SEXTIMM(add_ins) == 0x0003); }
 
 TEST_CASE("Task 3: OFF6 immediate operand extraction and sign extension", "[task3]")
-{
-  CHECK(OFF6(str_ins) == 0x001F);
-}
+{ CHECK(OFF6(str_ins) == 0x001F); }
 
 TEST_CASE("Task 3: PCOFF9 immediate operand extraction and sign extension", "[task3]")
-{
-  CHECK(PCOFF9(ld_ins) == 0xFF0F);
-}
+{ CHECK(PCOFF9(ld_ins) == 0xFF0F); }
 
 TEST_CASE("Task 3: PCOFF11 immediate operand extraction and sign extension", "[task3]")
-{
-  CHECK(PCOFF11(jsr_ins) == 0xFC55);
-}
+{ CHECK(PCOFF11(jsr_ins) == 0xFC55); }
 #endif // task3
 
 /**
