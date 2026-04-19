544b78b33adf5f7c1c27d9078f4920216d90423f
Bijaya Lama <blama4991@gmail.com>
2026-04-19 00:04:36 +0000

    Complete Task 2 sign extension function

--- include/lc3vm.h
+++ include/lc3vm.h
@@ -68,6 +68,7 @@ extern uint16_t PC_START;
 // your task functions should go here
 uint16_t mem_read(uint16_t address);
 void mem_write(uint16_t address, uint16_t value);
+uint16_t sign_extend(uint16_t bits, int size);
 
 void rti(uint16_t i);
 void res(uint16_t i);
