d2a85849215fbeb9393477df8ca32d4e19e63c31
Bijaya Lama <blama3@leomail.tamuc.edu>
2026-04-19 03:39:39 +0000

    Implemented ld(), ldi(), ldr() & lea() functions

--- include/lc3vm.h
+++ include/lc3vm.h
@@ -95,7 +95,10 @@ void update_flags(enum registr r);
 void add(uint16_t i);
 void andlc(uint16_t i);
 void notlc(uint16_t i);
-
+void ld(uint16_t i);
+void ldi(uint16_t i);
+void ldr(uint16_t i);
+void lea(uint16_t i);
 #ifdef TEST
 } // end extern C for C++ test runner
 #endif
